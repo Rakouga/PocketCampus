9695381997493647a9ff119ba25716d7ff2fd5f6
Rakouga <mx@HaojiaMacBook-Pro.local>
2016-05-29 15:13:48 +0800

    初步完成了postDetail的内容
    初步完成了postDetail的内容,cell也是自适应大小的.
    下一步需要完成回帖功能

--- PocketCampus/PostDetailViewController.swift
+++ PocketCampus/PostDetailViewController.swift
@@ -8,21 +8,31 @@
 
 import UIKit
 
-class PostDetailViewController: UIViewController ,UITableViewDataSource{
+class PostDetailViewController: UIViewController ,UITableViewDataSource,UITableViewDelegate{
 
     @IBOutlet weak var postDetailTableView: UITableView!
+    @IBOutlet weak var postTitleLabel: UILabel!
+    @IBOutlet weak var postUserNameLabel: UILabel!
+    @IBOutlet weak var postDateLabel: UILabel!
+    @IBOutlet weak var postContentLabel: UILabel!
+    //NSLayoutConstraint
+    @IBOutlet weak var postViewHeight: NSLayoutConstraint!
     
     var postID:String?
-    var post:Dictionary<String,String>!//保存帖子(1楼)的内容
     var repliesList = [Dictionary<String,String>]()//保存回帖列表
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.postDetailTableView.dataSource = self
-        
         self.getPost()
         self.getRepliesList()
+        self.postDetailTableView.dataSource = self
+        self.postDetailTableView.delegate = self
+        
+        self.postDetailTableView.estimatedRowHeight = 50
+        self.postDetailTableView.rowHeight = UITableViewAutomaticDimension
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,23 +46,37 @@ class PostDetailViewController: UIViewController ,UITableViewDataSource{
     */
     func getPost(){
         let query = BmobQuery(className: "Post")
+        
         query.getObjectInBackgroundWithId(self.postID) { (object, error) in
-            if error == nil{
-                print("getPost object:\(object)")
-                self.post["postID"] = object.objectForKey("objectId") as! String
-                self.post["userName"] = object.objectForKey("userName") as! String
-                self.post["content"] = object.objectForKey("content") as! String
-                self.post["date"] = object.objectForKey("createAt") as! String
-            }
+                print("正在getPost()")
+                if error == nil{
+                    print("getPost object:\(object)")
+                    self.postTitleLabel.text = (object as! BmobObject).objectForKey("title") as! String
+                    self.postUserNameLabel.text = (object as! BmobObject).objectForKey("userName") as! String
+                    self.postDateLabel.text = (object as! BmobObject).objectForKey("createdAt") as! String
+                    self.postContentLabel.text = (object as! BmobObject).objectForKey("content") as! String
+                    
+                    //根据文本来调整第一层的高度
+                    self.postContentLabel.sizeToFit();
+                    self.postViewHeight.constant = self.postContentLabel.frame.height + 70
+                    
+                }
+                else{
+                    print("getPost发生错误:\(error)")
+                }
         }
     }
     
+    /**
+     获取回帖列表
+     */
     func getRepliesList(){
         self.repliesList.removeAll()
         let query = BmobQuery(className: "Replies")
         query.orderByAscending("createdAt")
         query.whereKey("postID", equalTo: self.postID)
         query.findObjectsInBackgroundWithBlock { (array, error) in
+            print("离开getRepliesList()")
             if error == nil{
                 for data in array{
                     var dic = Dictionary<String, String>()
@@ -73,36 +97,31 @@ class PostDetailViewController: UIViewController ,UITableViewDataSource{
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0{
-            return 1;
-        }else{
             return self.repliesList.count;
-        }
     }
+    
+//    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+//        let cell = self.postDetailTableView.cellForRowAtIndexPath(indexPath) as! ReplyTableViewCell
+//        let label = cell.contentLabel
+//        label.sizeToFit()
+//        
+//        return label.frame.height + 40
+////        return 50
+//    }
+//    
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        if indexPath.section == 0 {
-            //返回帖子第一层
-            let cell = tableView.dequeueReusableCellWithIdentifier("postTableViewCell") as! PostTableViewCell
-            cell.postID = self.post["objectId"]
-            cell.userNameLabel.text = self.post["userName"]
-            cell.dateLabel.text = self.post["content"]
-            cell.contentLabel.text = self.post["date"]
-            
-            return cell
-        }else{
-            //返回回复的内容
-            let data = self.repliesList[indexPath.row]
-            let cell = tableView.dequeueReusableCellWithIdentifier("replyTableViewCell") as! ReplyTableViewCell
-            
-            cell.userNameLabel.text = data["userName"]
-            cell.contentLabel.text = data["reply"]
-            cell.dateLabel.text = data["date"]
-            cell.replyID = data["objectId"]
-            
-            return cell
-        }
+        //返回回复的内容
+        let data = self.repliesList[indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier("replyTableViewCell") as! ReplyTableViewCell
+        
+        cell.userNameLabel.text = data["userName"]
+        cell.contentLabel.text = data["reply"]
+        cell.dateLabel.text = data["date"]
+        cell.replyID = data["objectId"]
+        
+        return cell
     }
 
     @IBAction func back(sender: AnyObject) {
